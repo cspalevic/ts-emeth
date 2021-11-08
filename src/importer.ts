becd587f1b5e2f7b4219d7c1821d44c2d13836e1
Justin <juspears@paypal.com>
2021-11-08 10:17:50 -0800

    typescript 4.4 breaks this fixes

--- src/importer.ts
+++ src/importer.ts
@@ -2,10 +2,14 @@ import theme from 'emeth';
 
 type Component = Record<string, string>
 type Context = ((key: string) => any) & { keys(): string[] };
-type Theme = { [key: string]: Component };
+type Theme = Record<string, Component>;
+
+function isFunction(v: unknown): v is Function {
+    return typeof v === 'function';
+}
 
 function isContext(ctx: Context | Theme): ctx is Context {
-    return typeof ctx === 'function' && typeof ctx.keys === 'function';
+    return isFunction(ctx) && isFunction(ctx.keys);
 }
 
 export const importer = (ctx: Context | Theme) => theme(isContext(ctx) ? ctx.keys().reduce((ret: Theme, key) => {
